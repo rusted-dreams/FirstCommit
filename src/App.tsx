1d2f4b9bd1312820dc43201530b99509d4e25c4a
rusted-dreams <123829783+rusted-dreams@users.noreply.github.com>
2025-05-23 21:27:09 +0530

    add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <div className="bg-amber-400">hellooooo</div>
+      <Button>click me</Button>
     </>
   );
 }
